b2ff47d6a61dcd88ad2b4eff366af01ce4cd07d2
Gugun Gunawan <gugun.net21@gmail.com>
2024-04-15 09:40:16 +0700

    update seeder user dan auth login

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,18 +8,32 @@ use App\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+
         $users = [
             [
                 'email' => 'admin@store.com',
                 'name' => 'Admin One',
                 'password' => bcrypt('password123'),
                 'roles' => 'ADMIN',
+                'store_name' => 'Store One',
+                'categories_id' => 1,
+                'store_status' => '1',
+                'address_one' => 'Address One',
+                'address_two' => 'Address Two',
+                'provinces_id' => 1,
+                'regencies_id' => 1,
+                'zip_code' => '12345',
+                'country' => 'Country',
+                'phone_number' => '123456789'
+            ],
+            [
+                'email' => 'user@store.com',
+                'name' => 'Customer One',
+                'password' => bcrypt('password123'),
+                'roles' => 'User',
                 'store_name' => 'Store One',
                 'categories_id' => 1,
                 'store_status' => '1',
