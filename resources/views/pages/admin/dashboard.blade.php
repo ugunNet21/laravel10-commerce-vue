84840127881891bf066e1590d9719859aba0bbf5
Gugun Gunawan <gugun.net21@gmail.com>
2024-04-15 10:36:48 +0700

    add chart di admin

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -56,7 +56,42 @@
                     </div>
                 </div>
             </div>
+            <canvas id="myChart" width="400" height="200"></canvas>
+
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Customer', 'Revenue', 'Transaction'],
+            datasets: [{
+                label: 'Statistics',
+                data: [{{ $customer }}, {{ $revenue }}, {{ $transaction }}],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
+
 @endsection
