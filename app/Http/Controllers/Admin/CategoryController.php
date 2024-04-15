4423d9decda32504a4a4e85b1e4285bc4fbcdc6d
Gugun Gunawan <gugun.net21@gmail.com>
2024-04-15 08:32:32 +0700

    add sweetalert category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,24 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-
 use App\Category;
-
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CategoryRequest;
-
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Yajra\DataTables\Facades\DataTables;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         if (request()->ajax()) {
@@ -30,14 +23,14 @@ class CategoryController extends Controller
                     return '
                         <div class="btn-group">
                             <div class="dropdown">
-                                <button class="btn btn-primary dropdown-toggle mr-1 mb-1" 
-                                    type="button" id="action' .  $item->id . '"
-                                        data-toggle="dropdown" 
+                                <button class="btn btn-primary dropdown-toggle mr-1 mb-1"
+                                    type="button" id="action' . $item->id . '"
+                                        data-toggle="dropdown"
                                         aria-haspopup="true"
                                         aria-expanded="false">
                                         Aksi
                                 </button>
-                                <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">
+                                <div class="dropdown-menu" aria-labelledby="action' . $item->id . '">
                                     <a class="dropdown-item" href="' . route('category.edit', $item->id) . '">
                                         Sunting
                                     </a>
@@ -61,87 +54,59 @@ class CategoryController extends Controller
         return view('pages.admin.category.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('pages.admin.category.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(CategoryRequest $request)
     {
-        $data = $request->all();
+        try {
+            $data = $request->all();
 
-        $data['slug'] = Str::slug($request->name);
-        $data['photo'] = $request->file('photo')->store('assets/category', 'public');
+            $data['slug'] = Str::slug($request->name);
+            $data['photo'] = $request->file('photo')->store('assets/category', 'public');
 
-        Category::create($data);
+            Category::create($data);
 
-        return redirect()->route('category.index');
+            return redirect()->route('category.index')->withSuccess('Berhasil membuat  kategori baru!');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Data category gagal ditambahkan');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $item = Category::findOrFail($id);
 
-        return view('pages.admin.category.edit',[
-            'item' => $item
+        return view('pages.admin.category.edit', [
+            'item' => $item,
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(CategoryRequest $request, $id)
     {
-        $data = $request->all();
+        try {
+            $data = $request->all();
 
-        $data['slug'] = Str::slug($request->name);
-        $data['photo'] = $request->file('photo')->store('assets/category', 'public');
+            $data['slug'] = Str::slug($request->name);
+            $data['photo'] = $request->file('photo')->store('assets/category', 'public');
 
-        $item = Category::findOrFail($id);
+            $item = Category::findOrFail($id);
 
-        $item->update($data);
+            $item->update($data);
 
-        return redirect()->route('category.index');
+            return redirect()->route('category.index')->withSuccess('Kategori berhasil diupdate!');
+        } catch (\Exception $e) {
+            return redirect()->back()->withError('Terjadi Kesalahan !');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $item = Category::findorFail($id);
